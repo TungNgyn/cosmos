178cc26e86fd093abaa90ef66bd819ac0455d032
Tung <nguyen.tung@outlook.de>
2022-10-12 09:27:16 +0200

    initial

---
+++ javascript.js
@@ -0,0 +1,8 @@
+
+function changeZitat() {
+    document.getElementById('Zitat').innerHTML = '"Zwei Dinge sind unendlich, das Universum und die menschliche Dummheit, aber bei dem Universum bin ich mir noch nicht ganz sicher."';
+}
+
+function changeZitatBack() {
+    document.getElementById('Zitat').innerHTML = '"Das Unverständlichste am Universum ist im Grunde, dass wir es verstehen können."';
+}
